d7d7d2986d3d01b6213c64fe3dd00f062b560cef
mchung03 <minhee.chung14@gmail.com>
2023-08-28 16:13:58 -0700

    webpack manifest.json file

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -29,9 +29,30 @@ module.exports = () => {
         swSrc: './src-sw.js',
         swDest: 'src-sw.js',
       }),
+
+      // manifest.json file
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Text Editor',
+        short_name: 'Text',
+        description: 'A text editor that runs in the browser!',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      })
     ],
 
     module: {
+      // css loaders
       rules: [
         {
           test: /\.css$/i,
